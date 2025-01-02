0899836d837a12c063007728b4cd7e0c730d9b58
htshongany <htshongany@gmail.com>
2025-01-02 15:29:17 -0800

    update config

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
 import configparser
+import os
+
+# Obtenir le chemin absolu du répertoire contenant ce fichier
+base_path = os.path.dirname(os.path.abspath(__file__))
 
 # Créer un objet ConfigParser
-config = configparser.ConfigParser()
+config = configparser.ConfigParser(allow_no_value=True)
 
 # Lire le fichier de configuration
-config.read('config.ini')
+config.read(os.path.join(base_path, 'config.ini'))
 
 # Charger les variables
 DATABASE = config['variables'].get('DATABASE', 'todos.db')
 PID_FILE = config['variables'].get('PID_FILE', 'service.pid')
 SERVICE_NAME = config['variables'].get('SERVICE_NAME', 'UrlTodoListService')
 
-# Charger les modules
-MODULES = {str(i): module for i, (key, module) in enumerate(config['modules'].items())}
+# Charger les modules sans clés explicites
+MODULES = {str(i): module for i, module in enumerate(config['modules']) if module is not None}
